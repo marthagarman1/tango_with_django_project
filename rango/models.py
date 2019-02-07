012b8570f4ccb6ed1ece876f04fc6a979ac71dfc
Kaitlin <2402299g@student.gla.ac.uk>
2019-02-07 14:57:35 +0000

    Chapter 9.6
    User is able to load picture and register.

--- rango/models.py
+++ rango/models.py
@@ -1,5 +1,9 @@
 from django.db import models
 from django.template.defaultfilters import slugify
+from django.contrib.auth.models import User
+
+#attribute for max_length 
+
 
 class Category(models.Model):
 	name = models.CharField(max_length=128, unique=True)
@@ -30,4 +34,15 @@ class Page(models.Model):
 		return self.title 
 
 
+class UserProfile(models.Model):
+	# This line is required. Links UserProfile to a User model instance. 
+	user = models.OneToOneField(User)
 
+    # The additional attributes we wish to include.
+	website = models.URLField(blank=True)
+	picture = models.ImageField(upload_to='profile_images', blank=True)
+
+	# Override the __unicode__() method to return out something meaningful! 
+	# Remember if you use Python 2.7.x, define __unicode__ too!
+	def __str__(self):
+		return self.user.username
